f19eb64ac809e5cc2b12eb8a9a193a9fe2cec567
Julius Østerbye <julius310502@gmail.com>
2024-04-19 15:02:53 +0200

    gears template added + gears class and one instance of a gear in the json file

---
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Gear.java
@@ -0,0 +1,18 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+public class Gear extends FieldAction {
+
+    @Override
+    public boolean doAction(GameController gameController, Space space) {
+        Player player = space.getPlayer();
+        if (player != null) {
+            // Call the turnRight method from the GameController
+            gameController.turnRight(player);
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/GearTemplate.java
@@ -0,0 +1,6 @@
+package dk.dtu.compute.se.pisd.roborally.fileaccess.model;
+
+
+public class GearTemplate extends ActionTemplate {
+
+}
